23fbe6ba37e18445e503361ed346527dae698362
Mishieck <mishieckmwale@gmail.com>
2023-05-05 14:06:33 +0000

    feat(deps): add Deno assertions

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,4 +1,6 @@
 import * as format from 'https://deno.land/std@0.185.0/fmt/colors.ts';
 
 const { readDir } = Deno;
+
+export * from 'https://deno.land/std@0.185.0/testing/asserts.ts';
 export { format, readDir };
