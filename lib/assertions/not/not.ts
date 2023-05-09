23814a4023408db8b6d2bef8bf9ddc52223daadc
Mishieck <mishieckmwale@gmail.com>
2023-05-09 12:30:57 +0000

    docs: add documentation for

--- lib/assertions/not/not.ts
+++ lib/assertions/not/not.ts
@@ -1,9 +1,14 @@
 import type { Confirm } from '../../types/assert.types.ts';
 
+/**
+ * Inverts the result of an assertion.
+ * @param confirm - A callback for assertion.
+ * @returns `void`
+ */
 const not =
   (confirm: Confirm): Confirm =>
   (expectation) => {
-    const result = confirm(expectation);
+    const result = confirm(expectation); // TODO: Consider a case where confirm is async
     const passed = !result.passed;
     const message = passed ? undefined : 'Test failed';
     return { passed, message };
