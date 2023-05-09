9dbc2fe526a45b13b3f5fa8b8646061877ecc1e8
Mishieck <mishieckmwale@gmail.com>
2023-05-09 10:17:14 +0000

    feat(assertions): add useDenoAssertionAsync - An asynchronous version of .

--- lib/assertions/use-deno-assertion/use-deno-assertion.ts
+++ lib/assertions/use-deno-assertion/use-deno-assertion.ts
@@ -11,4 +11,20 @@ const useDenoAssertion = <DenoAssert extends (...args: Array<any>) => void>(
   }
 };
 
+const useDenoAssertionAsync = async <
+  // deno-lint-ignore no-explicit-any
+  DenoAssert extends (...args: Array<any>) => Promise<void>
+>(
+  assert: DenoAssert,
+  args: Parameters<DenoAssert>
+) => {
+  try {
+    await assert(...args);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 export default useDenoAssertion;
+export { useDenoAssertion, useDenoAssertionAsync };
