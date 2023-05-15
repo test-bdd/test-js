09b6e28e1a10498dd8b804deb3312889d34a3875
Mishieck <mishieckmwale@gmail.com>
2023-05-15 15:57:00 +0000

    docs(assertions): add documentation for toBeInstanceOf

--- lib/assertions/to-be-instance-of/to-be-instance-of.ts
+++ lib/assertions/to-be-instance-of/to-be-instance-of.ts
@@ -3,6 +3,12 @@ import type { Assert } from '../../types/assert.types.ts';
 import toString from '../../utils/to-string.ts';
 import useDenoAssertion from '../use-deno-assertion/use-deno-assertion.ts';
 
+/**
+ * Asserts if a given value is an instance of another value.
+ * @param result - A class or constructor.
+ * @returns `Confirm`; a function that takes the value passed to `expect`
+ *   and checks if it is an instance of `result`.
+ */
 const toBeInstanceOf: Assert = (result) => (expectation) => {
   const passed = useDenoAssertion(assertInstanceOf, [
     expectation,
