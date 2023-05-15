742fc85ce982ebc2e094f09a825d1ffb40b17a27
Mishieck <mishieckmwale@gmail.com>
2023-05-15 16:00:06 +0000

    docs(assertions): add documentation for toContainElement

--- lib/assertions/to-contain-element/to-contain-element.ts
+++ lib/assertions/to-contain-element/to-contain-element.ts
@@ -1,6 +1,12 @@
 import type { Assert } from '../../types/assert.types.ts';
 import toString from '../../utils/to-string.ts';
 
+/**
+ * Asserts if a given value is an element of an array.
+ * @param result - An array.
+ * @returns `Confirm`; a function that takes the value passed to `expect`
+ *   and checks if it is an element of `result`.
+ */
 const toContainElement: Assert = (result) => (expectation) => {
   const passed = (expectation as Array<unknown>).indexOf(result) > -1;
 
