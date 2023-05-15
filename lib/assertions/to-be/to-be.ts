21befdf9b470491c3e4edcb42768639adbd02c0b
Mishieck <mishieckmwale@gmail.com>
2023-05-15 15:53:41 +0000

    docs(assertions): add documentation for toBe

--- lib/assertions/to-be/to-be.ts
+++ lib/assertions/to-be/to-be.ts
@@ -1,5 +1,12 @@
 import type { Assert } from '../../types/assert.types.ts';
 
+/**
+ * Asserts the referential equality of two values.
+ * For primitive values, it has the same functionality as `toEqual`.
+ * @param result - The value to be compared to.
+ * @returns `Confirm`; a function that takes the value passed to `expect`
+ *   and compares it to `result`.
+ */
 const toBe: Assert = (result: unknown) => (expectation) => {
   if (result === expectation) {
     return {
